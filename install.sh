50cdff742fad118fcafa883bc0a91da8e4bbb403
ImAwsum <153707128+ImAwsumm@users.noreply.github.com>
2025-12-10 14:49:18 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,15 +7,19 @@ $ISYAY=/sbin/yay
 if [ -f $ISYAY ] ; then
 echo -e " Yay is nstalled, congrats \n"
 else 
-# yay is not installed 
-echo -e " yay is not installed, do you want to install it"
+# install yay if it isn't already installed
+
+echo -e "Yay is not installed, do you want to install it? \n"
 read -n1 '(Y/n)' YAY
 if [[ $YAY == "Y" || $YAY == "y" ]]; then
-git clone https://aur,archlinux.org/yay.git
-cd yay
-makepkg -si
+# install yay since it is needed to install the dependencies
+   git clone https://aur.archlinux.org/yay.git
+   cd yay
+   makepkg -si
 fi
 
-
 # install dependencies
-yay -S --noconfirm hyprland kitty waybar gtklock
+read -n1 -p 'Do you want to proceed with the installation of packages the packages? (Y/n)'
+yay -S --noconfirm hyprland kitty waybar gtklock hyprpaper wofi
+
+# actuallt install the dotfiles
