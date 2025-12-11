fd6975fd4a457fe1afc2eb42a866897a33080db0
ImAwsum <153707128+ImAwsumm@users.noreply.github.com>
2025-12-10 19:12:23 -0500

    Imported nvim init script
    This initialization script adds the Lazy nvim plugin and sets catppuccin as the default theme

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,1 +1,27 @@
+-- Load Lazy plugin manager
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  print("lazy.nvim not found")
+  return
+end
+vim.opt.rtp:prepend(lazypath)
 
+
+vim.wo.number = true
+vim.wo.relativenumber = true
+
+
+require("lazy").setup({
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+      require("catppuccin").setup({
+        flavour = "mocha", -- latte, frappe, macchiato, mocha
+        transparent_background = false,
+      })
+      vim.cmd.colorscheme "catppuccin"
+    end,
+  },
+})
