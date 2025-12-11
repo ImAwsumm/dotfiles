55efea557f9baf6760c665808eb28c54bab25b35
ImAwsum <153707128+ImAwsumm@users.noreply.github.com>
2025-12-11 08:46:57 -0500

    Fix yay installation prompt and dotfiles copy

--- install.sh
+++ install.sh
@@ -3,27 +3,28 @@
 # check if yay is installed
 cd ~
 ISYAY=/sbin/yay
-
 if [ -f $ISYAY ] ; then
-echo -e " Yay is nstalled, congrats \n"
-else
-# install yay if it isn't already installed
-read -n1 -p "Yay is not installed, do you want to install it? (Y/n): " YAY
-if [[ $YAY == "Y" || $YAY == "y" ]]; then
-# install yay since it is needed to install the dependencies
-   # check if makepkg is installed
 
-    if ! command -v makepkg &>/dev/null; then
+echo -e " Yay is nstalled, congrats \n"
+
+else
+   # install yay if it isn't already installed
+   read -n1 -p "Yay is not installed, do you want to install it? (Y/n): " YAY
+   if [[ $YAY == "Y" || $YAY == "y" ]]; then
+   # install yay since it is needed to install the dependencies
+   
+      # check if makepkg is installed
+      if ! command -v makepkg &>/dev/null; then
       echo -e "\nmakepkg is not installed. Installing 'base-devel' package group to proceed...\n"
       sudo pacman -S --noconfirm base-devel
-
+	  
       # Check if makepkg is available after installing base-devel
       if ! command -v makepkg &>/dev/null; then
         echo -e "makepkg installation failed. Please check your system configuration.\n"
         exit 1
       else
         echo -e "makepkg has been successfully installed!\n"
-      fi
+	  fi
     else
       echo -e "makepkg is already installed.\n"
     fi
@@ -44,5 +45,12 @@ read -n1 -p 'Do you want to proceed with the installation of packages? (Y/n) ' P
 if [[ $PKGINSTALL == "Y" || $PKGINSTALL == "y" ]]; then
 	yay -S --noconfirm hyprland kitty waybar gtklock hyprpaper wofi
 fi
-# actuallt install the dotfiles
+# actually install the dotfiles
+read -n1 -p 'Do you want to install the dotfiles? (Y/n) ' DOTINSTALL
+if [[ $DOTINSTALL == "Y" || $DOTINSTALL == "y" ]]; then
+cd ~
+cp hypr ~/.config/
+cp nvim ~/.config/
+fi
+
 cd ~
