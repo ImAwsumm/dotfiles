582025d889e3f2311fa18c9611daf88a4a6aef4c
ImAwsum <153707128+ImAwsumm@users.noreply.github.com>
2025-12-13 23:07:04 -0500

    Repair and upgrade update.sh script
    Added functionality to archive and save a copy of the old dotfiles before performing an update.

--- update.sh
+++ update.sh
@@ -2,6 +2,23 @@
 
 # check for current dotfiles version
 cd ~/.config/hypr
-
-AWSNVERSION=$(awk 'NR==1 {print; exit}' hyprland.conf)
+AWSNVERSION=$(awk 'NR==7 {print; exit}' hyprland.conf)
 echo -e " The current version for the dotfiles is $AWSMVERSION \n"
+cd ~
+
+read -n1 -p "Do you want to save a copy of your old dotfiles before updating them? (Y/n): " ARCHIVE 
+  if [[ $ARCHIVE == "Y" || $ARCHIVE == "y" ]]; then
+  cd ~/.config/hypr #   Create an Archive of the hyprland config files
+     mv hyprland.conf hyprland-old.conf
+     mv hyprpaper.conf hyprpaper-old.conf
+     mv hypridle.conf hypridle-old.conf
+  cd ~/.config/nvim #  Create an Archive of the NeoVim config files ( not needed )
+     mv init.lua init-old.lua
+  cd ~
+  fi
+
+# ask for update then apply update (incomplete)
+read -n1 -p "Do you want to update your dotfiles ? (Y/n): " UPDATE 
+    if [[ $UPDATE == "Y" || $UPDATE == "y" ]]; then
+    
+    fi
