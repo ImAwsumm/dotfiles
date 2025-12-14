e468d0b52f28034cda72a8f7ccc68070cfb9d969
ImAwsum <153707128+ImAwsumm@users.noreply.github.com>
2025-12-13 23:30:57 -0500

    Improve install.sh script with new backup feature +more
    Updated echo messages for more clarity and transparency.
    New archiving option available when installing the dotfiles.  This can be used in order to preserve the old dotfiles the user might want to keep.  
    Small fixes including but not limited to: typos, bug fixes, improved code clarity

--- install.sh
+++ install.sh
@@ -15,27 +15,27 @@ else
    
       # check if makepkg is installed
       if ! command -v makepkg &>/dev/null; then
-      echo -e "\nmakepkg is not installed. Installing 'base-devel' package group to proceed...\n"
+      echo -e "\nMakepkg is not installed. Installing 'base-devel' package group to proceed...\n"
       sudo pacman -S --noconfirm base-devel
 	  
       # Check if makepkg is available after installing base-devel
       if ! command -v makepkg &>/dev/null; then
-        echo -e "makepkg installation failed. Please check your system configuration.\n"
+        echo -e "Makepkg installation failed. Please check your system configuration.\n"
         exit 1
       else
-        echo -e "makepkg has been successfully installed!\n"
+        echo -e "Makepkg has been successfully installed!\n"
 	  fi
     else
-      echo -e "makepkg is already installed.\n"
+      echo -e "Makepkg is already installed.\n"
     fi
    git clone https://aur.archlinux.org/yay.git
    cd yay
    makepkg -si
    cd ..
-   echo -e "\nYay is nstalled, congrats!"
+   echo -e "\nYay is installed, congrats!"
 
    else
-      echo -e "\nYay is needed in order to proceed with the script"
+      echo -e "\nYay is needed in order to proceed with the script."
       exit 1
    fi
 fi
@@ -45,12 +45,41 @@ read -n1 -p 'Do you want to proceed with the installation of packages? (Y/n) ' P
 if [[ $PKGINSTALL == "Y" || $PKGINSTALL == "y" ]]; then
 	yay -S --noconfirm hyprland kitty waybar gtklock hyprpaper wofi
 fi
+
+# Propose saving the old config files before performing the update 
+
+read -n1 -p "Do you want to save a copy of your old dotfiles before updating them? (Y/n): " ARCHIVE 
+  if [[ $ARCHIVE == "Y" || $ARCHIVE == "y" ]]; then
+  
+  cd ~/.config/hypr #   Create an Archive of the hyprland config files
+     mv hyprland.conf hyprland-oldv0.conf
+     mv hyprpaper.conf hyprpaper-oldv0.conf
+     mv hypridle.conf hypridle-oldv0.conf
+	 
+  cd ~/.config/nvim #  Create an Archive of the NeoVim config files ( not needed )
+     mv init.lua init-oldv0.lua
+  cd ~
+  
+  echo -e "Done. -oldv0 was appended to the end of the old config filenames."
+  fi
+  
 # actually install the dotfiles
 read -n1 -p 'Do you want to install the dotfiles? (Y/n) ' DOTINSTALL
 if [[ $DOTINSTALL == "Y" || $DOTINSTALL == "y" ]]; then
-cd ~
-cp hypr ~/.config/
-cp nvim ~/.config/
+
+# install the hyprland dotfiles
+cd ~/dotfiles
+	cd hypr
+	cp hyprland.conf ~/.config/hypr # The (Actual) hyprland config file  
+	cp hypridle.conf  ~/.config/hypr
+	cp hyprpaper.conf ~/.config/hypr
+
+# install the neovim dotfiles
+	cd ~/dotfiles
+		cd ~/dotfiles/nvim
+		cp init.lua ~/.config/nvim 
+	cd ~/dotfiles
+
 fi
 
 cd ~
