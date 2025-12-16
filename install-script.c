0dfe92645d71642fdea272a193600c42911fa21a
ImAwsum <153707128+ImAwsumm@users.noreply.github.com>
2025-12-16 02:14:44 +0000

    Finished the c script (faster)
    I didn't add the hyprland (launch) at the end since it was useless and will not do anything good for our use

--- install-script.c
+++ install-script.c
@@ -1,33 +1,85 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#define MAX_CMD 512
 
-
-int main() {
-    // Check if yay is installed
-    if (system("test -f /sbin/yay") == 0)
+int main() 
 {
+    // Check if yay is installed
+    if (system("test -f /sbin/yay") == 0) 
+    {
         printf("Yay is now installed, congrats!\n");
-}
-      else 
-      {
+    }
+    else 
+    {
         char YAY;
         printf("Yay is not installed, do you want to install it? (Y/n): ");
-        scanf(" %c", &YAY);
-
-        if (YAY == 'Y' || YAY == 'y')
+        scanf(" %c", &YAY); // asks the user if they wanna install yay (needed) 
+        if (YAY == 'Y' || YAY == 'y') 
         {
-            // Check if makepkg is installed
-            if (system("command -v makepkg > /dev/null") != 0)
+            // Check if makepkg is installed ( it is needed in order to compile yay )
+            if (system("command -v makepkg > /dev/null") != 0) 
             {
                 printf("\nMakepkg is not installed. Installing 'base-devel' package group to proceed...\n");
                 system("sudo pacman -S --noconfirm base-devel");
 
-                // Check if makepkg is available after installing base-devel
-
-
-              // if makepkg is installed
-              // clone + compile + install yay 
-              // proceed with script
-            }          
+                // Check if makepkg is available after installing the base-devel package
+                if (system("command -v makepkg > /dev/null") != 0) 
+                {
+                    printf("Makepkg installation failed. Please check your system configuration.\n");
+                    return 1;
+                }
+                else 
+                {
+                    printf("Makepkg has been successfully installed!\n");
+                }
+            }
+            else 
+            {
+                printf("Makepkg is already installed.\n");
+            }
+            // Clone yay and build it from source
+            system("git clone https://aur.archlinux.org/yay.git");
+            system("cd yay && makepkg -si");
+            printf("\nYay is installed, congrats!\n");
         }
-  } 
+        else 
+        {
+            printf("\nYay is needed in order to proceed with the script.\n");
+            return 1;
+        }
+    }
+    // Install dependencies
+    char PKGINSTALL;
+    printf("Do you want to proceed with the installation of packages? (Y/n): ");
+    scanf(" %c", &PKGINSTALL);
+    printf("\n");
+    if (PKGINSTALL == 'Y' || PKGINSTALL == 'y') 
+    {
+        system("yay -S --noconfirm hyprland kitty waybar gtklock hyprpaper wofi");
+    }
+    // Propose saving the old config files before performing the update
+    char ARCHIVE;
+    printf("Do you want to save a copy of your old dotfiles before applying the new config? (Y/n): ");
+    scanf(" %c", &ARCHIVE);
+    if (ARCHIVE == 'Y' || ARCHIVE == 'y')
+    {
+        system("cd ~/.config/hypr && mv hyprland.conf hyprland-oldv0.conf && mv hyprpaper.conf hyprpaper-oldv0.conf && mv hypridle.conf hypridle-oldv0.conf");
+        system("cd ~/.config/nvim && mv init.lua init-oldv0.lua");
+        printf("Done. -oldv0 was appended to the end of the old config filenames.\n");
+    }
+    // Install the dotfiles
+    char DOTINSTALL;
+    printf("Do you want to install the dotfiles? (Y/n): ");
+    scanf(" %c", &DOTINSTALL);
+    if (DOTINSTALL == 'Y' || DOTINSTALL == 'y') 
+    {
+        system("cd ~/dotfiles/hypr && cp hyprland.conf ~/.config/hypr && cp hypridle.conf ~/.config/hypr && cp hyprpaper.conf ~/.config/hypr");
+        system("cd ~/dotfiles/nvim && cp init.lua ~/.config/nvim");
+    }
+        else 
+    {
+        return 0;
+    }
+    return 0;
+}
