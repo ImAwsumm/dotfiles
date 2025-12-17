16947c9021e41d2c82c986cba9fe534c84d828e6
ImAwsum <153707128+ImAwsumm@users.noreply.github.com>
2025-12-17 04:02:00 +0000

    Create update-script.c

---
+++ update-script.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_CMD 512
+// error code 1 is caused by a missing dependency
+int main() 
+{
+system("cd ~/.config/hypr");
+  // check the current version in the hyprland config and match it to the (current one)
+
+  
+  return 0;
+}
