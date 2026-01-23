a0b2a44e0909576967df746348b718411230d89e
ImAwsum <153707128+ImAwsumm@users.noreply.github.com>
2026-01-22 21:57:02 -0500

    performance improvement

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,12 +2,12 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
-    lazypath,
+      "git",
+      "clone",
+      "--filter=blob:none",
+      "https://github.com/folke/lazy.nvim.git",
+      "--branch=stable",
+      lazypath,
   })
 end
 vim.opt.rtp:prepend(lazypath)
@@ -15,6 +15,10 @@ vim.opt.rtp:prepend(lazypath)
 vim.opt.clipboard = "unnamedplus"
 vim.wo.number = true
 vim.wo.relativenumber = true
+
+vim.opt.tabstop = 8
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
 
 require("lazy").setup({
  	 {
@@ -29,54 +33,29 @@ require("lazy").setup({
  	     vim.cmd.colorscheme "catppuccin"
  	   end,
  	 },
-	 -- lazy.nvim
-	{
-	  "folke/snacks.nvim",
-	  ---@type snacks.Config
-	  opts = {
-	    indent = {
-	      -- your indent configuration comes here
-	      -- or leave it empty to use the default settings
-	      -- refer to the configuration section below
-	    }
-	  }
-	},
-  	{ -- telescope 
-  	  'nvim-telescope/telescope.nvim', tag = '0.1.8',
-  	  dependencies = {
-  	      'nvim-lua/plenary.nvim',
-  	      { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
-  	  }
-  	},-- lazy.nvim
-{
-    "lukas-reineke/indent-blankline.nvim",
-    main = "ibl",
-    ---@module "ibl"
-    ---@type ibl.config
-    opts = {},
-},
-	{
-  'stevearc/conform.nvim',
-  opts = {},
-},
-	{
+	 {
+	   "folke/snacks.nvim",
+	   ---@type snacks.Config
+	   opts = {
+	     indent = {
+	       -- your configuration comes here
+	       -- or leave it empty to use the default settings
+	     }
+	   }
+	 },
+  	 { -- telescope 
+  	   'nvim-telescope/telescope.nvim', tag = '0.1.8',
+  	   dependencies = {
+  	       'nvim-lua/plenary.nvim',
+  	       { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+  	   }
+  	 },
+	 {
 	    'nvim-lualine/lualine.nvim',
 	    dependencies = { 'nvim-tree/nvim-web-devicons' }
-	}
+	 }
 })
+
 local builtin = require("telescope.builtin")
 vim.keymap.set('n', '<C-p>', builtin.find_files, {})
 require('lualine').setup()
-options = { theme = 'gruvbox-material' }
-
-require("conform").setup({
-  formatters_by_ft = {
-    lua = { "stylua" },
-    -- Conform will run multiple formatters sequentially
-    python = { "isort", "black" },
-    -- You can customize some of the format options for the filetype (:help conform.format)
-    rust = { "rustfmt", lsp_format = "fallback" },
-    -- Conform will run the first available formatter
-    javascript = { "prettierd", "prettier", stop_after_first = true },
-  },
-})
