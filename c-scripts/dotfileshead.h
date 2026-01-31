ffd4a2730e11dfbe230b36b86430eec52fe97eda
ImAwsum <153707128+ImAwsumm@users.noreply.github.com>
2026-01-30 22:18:09 -0500

    create header file
    not currently used by anything

---
+++ c-scripts/dotfileshead.h
@@ -0,0 +1,38 @@
+#define HSETUP_DOT_H
+
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#define BOLD_S  	"\e[1m" // defines BOLD_S as a keyword to make text bold
+#define UDRL_S  	"\e[4m" // UDRL_S starts an underline style
+#define ITALICS_S 	"\e[3m"
+#define ANSI_BLUE    	"\x1b[34m"
+#define ANSI_WHITE    	"\x1b[97m"
+#define ANSI_GREY    	"\x1b[90m"
+#define ANSI_CYAN    	"\x1b[36m"
+#define ANSI_RED 	"\x1b[31m"
+#define STYLE_END   	"\e[m" // resets the styling
+
+void clear();
+char ARCHIVE;
+char PKGINSTALL;
+char full_install_opt; // if the user wants to install everything set to Y
+float pver; // the user is presumed to be installing the dotfiles
+
+void BASH(char ARCHIVE, float pver, char PKGINSTALL);
+void SWAY(char ARCHIVE, float pver, char PKGINSTALL);
+void BTOP(char ARCHIVE, float pver, char PKGINSTALL);
+void CAVA(char ARCHIVE, float pver, char PKGINSTALL);
+void FAST(char ARCHIVE, float pver, char PKGINSTALL);
+void FUZZ(char ARCHIVE, float pver, char PKGINSTALL);
+void GTKL(char ARCHIVE, float pver, char PKGINSTALL);
+void HYPR(char ARCHIVE, float pver, char PKGINSTALL);
+void KITT(char ARCHIVE, float pver, char PKGINSTALL);
+void NVIM(char ARCHIVE, float pver, char PKGINSTALL);
+void WAYB(char ARCHIVE, float pver, char PKGINSTALL);
+
+void full_install(char ARCHIVE, char full_install_opt);
+float* update();
