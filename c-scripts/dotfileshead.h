9edfc0494024a5eef8be1d2472f0bb07c5832aba
ImAwsum <153707128+ImAwsumm@users.noreply.github.com>
2026-02-04 22:10:38 -0500

    add template text for every package

--- c-scripts/dotfileshead.h
+++ c-scripts/dotfileshead.h
@@ -44,6 +44,23 @@ void WAYB(char ARCHIVE, float pver, char PKGINSTALL);
 void full_install(char ARCHIVE, char full_install_opt);
 int full_update(char ARCHIVE, float pver);
 
+const int max_menu_opt_n = 12;
+
 void install_configs(float pver); // the partial install script (configure which package or configuration to install)
 
 float* update();
+
+char* TEXT_C_BASH = ".bashrc";
+char* TEXT_C_SWAY = "Sway-WM configs";
+char* TEXT_C_BTOP = "btop config";
+char* TEXT_C_CAVA = "cava config";
+char* TEXT_C_FAST = "fastfetch configs";
+char* TEXT_C_FUZZ = "fuzzel configs";
+char* TEXT_C_GTKL = "gtklock config and style (appearance) ";
+char* TEXT_C_HYPR = "Hyprland-WM config + Hypridle config";
+char* TEXT_C_KITT = "kitty terminal";
+char* TEXT_C_MPVF = "mpv config";
+char* TEXT_C_NVIM = "neovim config with lazy";
+char* TEXT_C_WAYB = "waybar config and style (appearance)";
+
+
