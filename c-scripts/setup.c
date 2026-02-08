6addd418366f7ed1ca16315217c704731e070027
ImAwsum <153707128+ImAwsumm@users.noreply.github.com>
2026-02-07 23:56:55 -0500

    remove the text defined twice

--- c-scripts/setup.c
+++ c-scripts/setup.c
@@ -635,19 +635,6 @@ void full_install(char ARCHIVE, char full_install_opt)
 	int install_pkg_opt;
 	do
 	{
-	    char TEXT_C_BASH = ".bashrc";
-            char TEXT_C_SWAY = "Sway-WM configs";
-            char TEXT_C_BTOP = "btop config";
-            char TEXT_C_CAVA = "cava config";
-            char TEXT_C_FAST = "fastfetch configs";
-            char TEXT_C_FUZZ = "fuzzel configs";
-            char TEXT_C_GTKL = "gtklock config and style (appearance) ";
-            char TEXT_C_HYPR = "Hyprland-WM config + Hypridle config";
-            char TEXT_C_KITT = "kitty terminal";
-            char TEXT_C_MPVF = "mpv config";
-            char TEXT_C_NVIM = "neovim config with lazy";
-            char TEXT_C_WAYB = "waybar config and style (appearance)";
-
 	    printf("\n[1] Install BASH ");
 	    printf("\n[2] Install SWAY ");
 	    printf("\n[3] Install BTOP ");
