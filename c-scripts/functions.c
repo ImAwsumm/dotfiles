32ffc88697221b2a24bf3bbc72e19e2dd622d171
ImAwsum <153707128+ImAwsumm@users.noreply.github.com>
2026-02-15 21:27:48 -0500

    Update functions.c

--- c-scripts/functions.c
+++ c-scripts/functions.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <time.h>
 
 const int max_menu_opt_n = 12;
 
@@ -20,3 +21,10 @@ char* TEXT_C_MPVF = "mpv config";
 char* TEXT_C_NVIM = "neovim config with lazy";
 char* TEXT_C_WAYB = "waybar config and style (appearance)";
 
+char ARCHIVE;
+char PKGINSTALL;
+char full_install_opt; // if the user wants to install everything set to Y
+char full_update_opt; 
+
+struct timespec install_timer;
+int fastfetch_conf_export;
