f2f9a4cd413a5c381367d1f94e5a7ac4622ac102
ImAwsum <153707128+ImAwsumm@users.noreply.github.com>
2026-02-15 21:08:15 -0500

    Update functions.c

--- c-scripts/functions.c
+++ c-scripts/functions.c
@@ -6,3 +6,17 @@ void clear()
 {
     printf("\033[2J\033[H");
 }
+
+char* TEXT_C_BASH = ".bashrc";
+char* TEXT_C_SWAY = "Sway-WM configs";
+char* TEXT_C_BTOP = "btop config";
+char* TEXT_C_CAVA = "cava config";
+char* TEXT_C_FAST = "fastfetch configs";
+char* TEXT_C_FUZZ = "fuzzel configs";
+char* TEXT_C_GTKL = "gtklock config and style (appearance) ";
+char* TEXT_C_HYPR = "Hyprland-WM config + Hypridle config";
+char* TEXT_C_KITT = "kitty terminal";
+char* TEXT_C_MPVF = "mpv config";
+char* TEXT_C_NVIM = "neovim config with lazy";
+char* TEXT_C_WAYB = "waybar config and style (appearance)";
+
