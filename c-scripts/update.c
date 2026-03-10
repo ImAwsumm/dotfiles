177a1a8b60a67dee2603e2d45061abe37834f758
ImAwsum <153707128+ImAwsumm@users.noreply.github.com>
2026-03-10 12:05:03 -0400

    Add error code for unknown version

--- c-scripts/update.c
+++ c-scripts/update.c
@@ -119,10 +119,9 @@ int full_update(char ARCHIVE, float pver)
 	wait_for_timeout(2, 0);
 	goto exit;
 
-    default:
-	printf("\nUnknown version\n");
-	wait_for_timeout(2, 0);
-	return 1;
+	default:
+		error_message(207);
+		return 1;
     end:
 	printf("Update completed!\n");
 	goto exit;
