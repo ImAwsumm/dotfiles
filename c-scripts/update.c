538d09012e54afdf97e2c23fc35ffc86fbf3aaa9
ImAwsum <153707128+ImAwsumm@users.noreply.github.com>
2026-03-16 21:52:14 -0400

    last commit to the incremental update
    this will be deprecated since it's not optimal and it is very hard to maintain with time

--- c-scripts/update.c
+++ c-scripts/update.c
@@ -11,22 +11,20 @@ int full_update(char ARCHIVE, float pver)
 
 	install_package(parent, "cava fuzzel kitty fastfetch waybar");
 
-	snprintf(cmd, 128,
-		"cp -f kitty/kitty.conf ~/.config/kitty ; "
-		"cp -f kitty/current-theme.conf ~/.config/current-theme.conf ");
-	system(cmd);
-	snprintf(cmd, 96,
-		"cp hypr/hypridle.conf ~/.config/hypr ; "
-		"cp hypr/hyprpaper.conf ~/.config/hypr ");
+	snprintf(cmd, 1024,
+		"cp -f %s/kitty/kitty.conf ~/.config/kitty ; "
+		"cp -f %s/kitty/current-theme.conf ~/.config/current-theme.conf ; "
+		"cp -f %s/hypr/hypridle.conf ~/.config/hypr ; "
+		"cp -f %s/hypr/hyprpaper.conf ~/.config/hypr ", inpath, inpath, inpath, inpath);
 	system(cmd);
 	__attribute__ ((fallthrough));
 	// do not break because we are also installing everything below
     case 120:
-	snprintf(cmd, 256,
-		"cp -f fuzzel/fuzzel.ini ~/.config/fuzzel/ ; "
-		"cp -f fuzzel/old-fuzzel.ini ~/.config/fuzzel/ ; "
-		"cp waybar/style.css ~/.config/waybar ; "
-		"cp waybar/config.jsonc ~/.config/waybar");
+	snprintf(cmd, 512,
+		"cp -f %s/fuzzel/fuzzel.ini ~/.config/fuzzel/ ; "
+		"cp -f %s/fuzzel/old-fuzzel.ini ~/.config/fuzzel/ ; "
+		"cp -f %s/waybar/style.css ~/.config/waybar ; "
+		"cp -f %s/waybar/config.jsonc ~/.config/waybar", inpath, inpath, inpath, inpath);
 	system(cmd);
 	__attribute__ ((fallthrough));
 	// do not break because we are also installing everything below
@@ -58,35 +56,31 @@ int full_update(char ARCHIVE, float pver)
 	// do not break because we are also installing everything below
     case 200:
 	install_package(parent, "gtklock");
-	snprintf(cmd, 320,
-		"mkdir ~/.config/gtklock ; "
-		"mv ~/.config/gtklock/style.css ~/.config/gtklock/style-v%.2f.css ; "	// gtklock exporting
-		"cp -f %s/gtklock/style.css ~/.config/gtklock/style.css ", pver, inpath);
+	snprintf(cmd, 24,
+		"mkdir ~/.config/gtklock");
 	system(cmd);
 
 	__attribute__ ((fallthrough));
 	// do not break because we are also installing everything below
     case 210:
 	install_package(parent, "sway");
-	snprintf(cmd, 512,
+	snprintf(cmd, sizeof(cmd),
 		"mkdir -p ~/.config/sway ; "
 		"mkdir -p ~/.config/nvim ; "
-		"mv ~/.config/nvim/init.lua ~/.config/nvim/init-oldv%.2f.lua ; "
-		"cp -f nvim/init.lua ~/.config/nvim ; "
 		"mv ~/.config/gtklock/style.css ~/.config/gtklock/style-v%.2f.css ; "
-		"cp -f gtklock/style.css ~/.config/gtklock/style.css ; "
+		"cp -f %s/gtklock/style.css ~/.config/gtklock/style.css ; "
+		"cp -f %s/kitty/kitty.conf ~/.config/kitty/ ; "	// update kitty config
 		"mv ~/.config/kitty/kitty.conf ~/.config/kitty/kitty-oldv%.2f.conf ; "
-		"cp -f kitty/kitty.conf ~/.config/kitty/ ; "	// update kitty config
-		, pver, pver, pver);
+		, pver, inpath, inpath, pver);
 	system(cmd);
 	__attribute__ ((fallthrough));
 	// do not break because we are also installing everything below
     case 220:
 	snprintf(cmd, 512,
+		"mkdir -p ~/.config/mpv/ ; "
 		"mv ~/.config/sway/config ~/.config/sway/config-oldv%.2f ; "
 		"cp -f %s/sway/config ~/.config/sway/ ; "	// update sway config
 		"cp -f %s/sway/config-default ~/.config/sway/ ; "
-		"mkdir -p ~/.config/mpv/ ; "
 		"mv ~/.config/mpv/mpv.conf ~/.config/mpv/mpv-oldv%.2f.conf ; "
 		"cp -f %s/mpv/mpv.conf ~/.config/mpv/ ", pver, inpath, inpath, pver, inpath);
 	system(cmd);
@@ -110,6 +104,7 @@ int full_update(char ARCHIVE, float pver)
 	system(cmd);
 	goto end;
     case 240:
+	install_package(parent, "bpytop");
 	snprintf(cmd, sizeof(cmd),
 		"cp -f %s/bpytop/bpytop.conf ~/.config/bpytop/ ", inpath);
 	system(cmd);
