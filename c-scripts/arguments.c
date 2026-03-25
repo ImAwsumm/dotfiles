bf8de55ed69946bac64a303cef011052ee0bedea
ImAwsumm <153707128+ImAwsumm@users.noreply.github.com>
2026-03-24 22:16:50 -0400

    use error_message(302)
    for command line errors

--- c-scripts/arguments.c
+++ c-scripts/arguments.c
@@ -4,6 +4,7 @@ void cli_arg_missing(char *first_command, char *type_of_missing_arg, char *user_
 {
     // prints an error message if there isn't a package specified in the command
     printf(BOLD_S ANSI_RED"%s: missing %s after -- '%s'\n"STYLE_END, first_command, type_of_missing_arg, user_flag_t);
+    error_message(302);
 }
 
 int parse_arguments(int num_cmd_arguments, char *cmd_arg_v[])
