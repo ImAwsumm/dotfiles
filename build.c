109e348a0970ff45c17fde5fdd3f7b3b90c68149
ImAwsumm <153707128+ImAwsumm@users.noreply.github.com>
2026-03-31 21:08:26 -0400

    move compiler detection at the start &more
    also add clang message
    fix other compiler messages

--- build.c
+++ build.c
@@ -1,5 +1,6 @@
-#include <stdio.h>
 #include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 bool verbose_log_output;
@@ -17,16 +18,6 @@ int main(int argc, char *argv[])
     // declare compiler_name enum
     compiler_enum compiler_name;
 
-    switch(compiler_name)
-    {
-	case ZIG:
-	    printf("zig compiler\n");
-	    break;
-        default: 
-	    printf("not special\n");
-	    break;
-    }
-
     if (strcmp(argv[1], "gcc") == 0)
     {
 	compiler_name = GCC;
@@ -40,6 +31,22 @@ int main(int argc, char *argv[])
     {
 
 	verbose_log_output = true;
+    }
+
+    switch(compiler_name)
+    {
+	case CLANG:
+	    printf("Clang compiler\n");
+	    break;
+	case GCC:
+	    printf("GCC compiler\n");
+	    break;
+	case ZIG:
+	    printf("zig compiler\n");
+	    break;
+        default: 
+	    printf("Unknown compiler\n");
+	    return 1;
     }
 
     return 0;
