730ed4a259e2e3ff1bca602554a1c4fd5486e145
ImAwsumm <153707128+ImAwsumm@users.noreply.github.com>
2026-03-31 20:55:05 -0400

    create basic parsing for compiler
    creates enums for the compiler_names

--- build.c
+++ build.c
@@ -1,1 +1,35 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
 
+typedef enum 
+{
+    CLANG,
+    ZIG,
+    GCC
+
+} compiler_enum;
+
+int main(int argc, char *argv[])
+{
+    // declare compiler_name enum
+    compiler_enum compiler_name;
+
+    switch(compiler_name)
+    {
+	case ZIG:
+	    printf("zig compiler\n");
+	    break;
+        default: 
+	    printf("not special\n");
+	    break;
+    }
+
+    if (strcmp(argv[1], "gcc") == 0)
+    {
+	compiler_name = GCC;
+    }
+
+    return 0;
+
+}
