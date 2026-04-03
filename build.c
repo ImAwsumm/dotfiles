420f5c4af87ab25778dad5ebb0c365c4e696ad1c
ImAwsumm <153707128+ImAwsumm@users.noreply.github.com>
2026-04-03 02:23:32 -0400

    Fix weird buffer sizes and magic numbers
    Add ~precise* buffer size calculation in order to save on ram because it's getting pretty expensive these days
    This also avoids all buffer overflows.  The user imput is never trusted for anything and it can never cause any buffer overflow.

--- build.c
+++ build.c
@@ -3,13 +3,15 @@
 #include <stdbool.h>
 #include <string.h>
 
-#define CMD_MAX 24
+#define COMPILER_NAME_SIZE 8
 
 void compile_all_files(char *compiler, char *flags);
 void link_object_files(char *compiler, char *flags);
 
-char *object_fpath = "c-scripts";
-char *source_fpath = "c-scripts";
+char object_fpath[12] = "c-scripts";
+char source_fpath[12] = "c-scripts";
+
+int size_source_filename = 24;
 
 const char *source_files[] = { 
     "arguments",
@@ -32,7 +34,7 @@ typedef enum
 
 int main(int argc, char *argv[])
 {
-    char compiler_name_cmd[CMD_MAX];
+    char compiler_name_cmd[COMPILER_NAME_SIZE];
     compiler_name_cmd[0] = '\0';
 
     // declare compiler_name enum
@@ -71,13 +73,13 @@ int main(int argc, char *argv[])
     switch(compiler_name)
     {
 	case CLANG:
-	    strncpy(compiler_name_cmd, "clang", CMD_MAX - 1);
+	    strncpy(compiler_name_cmd, "clang", COMPILER_NAME_SIZE - 1);
 	    break;
 	case GCC:
-	    strncpy(compiler_name_cmd, "gcc", CMD_MAX - 1);
+	    strncpy(compiler_name_cmd, "gcc", COMPILER_NAME_SIZE - 1);
 	    break;
 	case ZIG:
-	    strncpy(compiler_name_cmd, "zig cc", CMD_MAX - 1);
+	    strncpy(compiler_name_cmd, "zig cc", COMPILER_NAME_SIZE - 1);
 	    break;
         default: 
 	    printf("Unknown compiler\n");
@@ -117,23 +119,32 @@ void compile_all_files(char *compiler, char *flags)
 
 void link_object_files(char *compiler, char *flags)
 {
-
-    char source_files_obj_cmd[512];
-    source_files_obj_cmd[0] = '\0';
-    char link_cmd[128];
-
+    // define the memory needed for the command buffer
+    int buffer_size_flags = snprintf(NULL, 0,
+	    "%s -o -c", flags);
     ssize_t num_src_files = (sizeof(source_files) / sizeof(source_files[0]) -1 );
 
-    char temp_obj_path[64];
+    int size_obj_fpath = sizeof(object_fpath) + size_source_filename + 8;
+    char temp_obj_path[size_obj_fpath];
+
+    // set object buffer size based on the size of path+filename
+    int obj_buffer_size = sizeof(temp_obj_path) * num_src_files;
+
+    char source_files_obj_cmd[obj_buffer_size];
+    source_files_obj_cmd[0] = '\0';
+
+    // calculate total command buffer size
+    // this avoids all buffer overflows since it is based on the size of 
+    // all the strings composing this command
+    int link_cmd_size = COMPILER_NAME_SIZE + obj_buffer_size + buffer_size_flags;
+    char link_cmd[link_cmd_size];
 
     for (int i = 0; num_src_files > i; i++)
     {
-	snprintf(temp_obj_path, sizeof(temp_obj_path),
+	snprintf(temp_obj_path, size_obj_fpath,
 		"%s/%s.o ", object_fpath, source_files[i]);
 	strcat(source_files_obj_cmd, temp_obj_path);
     }
-
-    //strcat(link_cmd, source_files_obj_cmd);
 
     snprintf(link_cmd, sizeof(link_cmd),
 	    "%s %s %s", compiler, source_files_obj_cmd, flags);
