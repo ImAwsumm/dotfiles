9cd09cb99b6433bf02f10eefbad966cf1b6c3626
ImAwsumm <153707128+ImAwsumm@users.noreply.github.com>
2026-04-15 11:20:29 -0400

    feat: template enums

--- c-scripts/enums.h
+++ c-scripts/enums.h
@@ -19,3 +19,35 @@ typedef enum
     LATEST = V_3_2,
     NEXT = V_3_3,
 } df_version;
+
+typedef enum 
+{
+    CONF_UNKNOWN,
+    CONF_BASH,
+    CONF_BTOP,
+    CONF_BPYT,
+    CONF_CAVA,
+    CONF_FAST,
+    CONF_FUZZ,
+    CONF_GTKL,
+    CONF_HYPR,
+    CONF_KITT,
+    CONF_MPVF,
+    CONF_NVIM,
+    CONF_SWAY,
+    CONF_WAYB,
+    CONF_ZSHH
+} pkg_conf_name;
+
+typedef enum
+{
+    INVALID_INPUT = 2,
+    YAY_INST_U = 5,
+}
+   
+    
+
+extern enum error_code_e;
+
+// error_code_e error_message_code = err_code;
+
