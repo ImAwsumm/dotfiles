d7c3488c560df6edd0ef11547796363ed38b456f
emile-ross <emileross@tutanota.de>
2026-08-24 13:33:47 -0400

    fix: compare.c missing long flag comparaison
    this now compares the short and the long flag to the original argument

--- src/compare.c
+++ src/compare.c
@@ -12,7 +12,11 @@ bool scmp(const char *arg, const char *flag)
 
 bool cmp(const char *arg, const char *s_flag, const char *l_flag)
 {
-	if (strcmp(arg, flag) == 0)
+	if (strcmp(arg, s_flag) == 0)
+	{
+		return true;
+	}
+	else if (strcmp(arg, l_flag) == 0)
 	{
 		return true;
 	}
