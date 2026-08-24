bb36f237bae2c397232bd35fd7cb398c91794e9f
emile-ross <emileross@tutanota.de>
2026-08-24 13:49:22 -0400

    refactor: improve structure clarity
    this makes the code cleaner by checking a condition and then exiting if
    it is false

--- src/install.c
+++ src/install.c
@@ -220,42 +220,40 @@ void check_for_yay(void)
         	scanf(" %c", &YAY); /* asks the user if they wanna install yay (needed) */
 		bool install_yay = y_n(YAY); /* convert the Y/n into a bool with the y_n() function */
 
-		if (install_yay)
+		if (!install_yay)
 		{
-			/* Check if makepkg is installed ( it is needed in order to compile yay ) */
+			error_message(YAY_INST_U);
+			exit(1);
+		}
+		/* Check if makepkg is installed ( it is needed in order to compile yay ) */
+		if (system("command -v makepkg > /dev/null") != 0)
+		{
+			printf("\nMakepkg is not installed. Installing 'base-devel' package group to proceed...\n");
+			exec_cmd(48, "sudo pacman -S --noconfirm base-devel");
+		
+			/* Check if makepkg is available after installing the base-devel package */
 			if (system("command -v makepkg > /dev/null") != 0)
 			{
-				printf("\nMakepkg is not installed. Installing 'base-devel' package group to proceed...\n");
-				exec_cmd(48, "sudo pacman -S --noconfirm base-devel");
-			
-				/* Check if makepkg is available after installing the base-devel package */
-				if (system("command -v makepkg > /dev/null") != 0)
-				{
-					error_message(MAKEPKG_FAIL);
-				}
-				else
-				{
-					printf("Makepkg has been successfully installed!\n");
-				}
+				error_message(MAKEPKG_FAIL);
 			}
 			else
 			{
-				printf("Makepkg is already installed.\n");
-				exec_cmd(48, "sudo pacman -S --noconfirm base-devel"); /* update base-devel */
+				printf("Makepkg has been successfully installed!\n");
 			}
-			
-			char cmd[256];
-			snprintf(cmd, sizeof(cmd),
-					"git clone https://aur.archlinux.org/yay.git ; "	/* download yay from aur */
-					"cd yay ; "		
-					"makepkg -si ; " /* build package from source */
-					"cd ..");		
-			system(cmd);
-			printf("\nYay is installed, congrats!\n");
 		}
 		else
 		{
-			error_message(YAY_INST_U);
+			printf("Makepkg is already installed.\n");
+			exec_cmd(48, "sudo pacman -S --noconfirm base-devel"); /* update base-devel */
 		}
+		
+		char cmd[256];
+		snprintf(cmd, sizeof(cmd),
+				"git clone https://aur.archlinux.org/yay.git ; "	/* download yay from aur */
+				"cd yay ; "		
+				"makepkg -si ; " /* build package from source */
+				"cd ..");		
+		system(cmd);
+		printf("\nYay is installed, congrats!\n");
 	}
 }
